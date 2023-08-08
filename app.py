146172fcf3fe0b8133d015da821252ab9ef79917
Nikunj <nikunjthakor2001@gmail.com>
2023-08-08 13:25:06 -0400

    data from url

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask, jsonify, request, render_template 
+import requests as re
+import json
+
 app = Flask(__name__,template_folder='Templates')
 
 @app.route('/') 
 def index(): 
     return render_template('index.html')
 
+@app.route('/details', methods=['GET'])
+def details():
+    req = re.get('https://api.coinlore.net/api/tickers/')
+    data = json.loads(req.content)
+    return render_template('table.html', data=data['data'])
+
 @app.route('/google-charts/pie-chart') 
 def google_pie_chart(): 
-    data = {'Task' : 'Hours per Day', 'Work' : 11, 'Eat' : 2, 'Commute' : 2, 'watching TV' : 2, 'Sleeping' : 7} 
-    return render_template('pie-chart.html', data=data)
+    req = re.get('https://api.coinlore.net/api/tickers/')
+    data = json.loads(req.content)    
+    return render_template('pie-chart.html', data=data['data'])
 
 if __name__ == '__main__':
     app.run()
